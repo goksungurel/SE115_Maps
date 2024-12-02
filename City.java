e77d7d85ad6abc272132ba3808040c0b993c4a84
goksungurel <148645864+goksungurel@users.noreply.github.com>
2024-12-03 01:05:28 +0300

    Update City.java
    create a constructor and addRoute() method

--- City.java
+++ City.java
@@ -1,6 +1,39 @@
 public class City{
-private String name;
-
-public City(String name){
-this.name=name;
+   private String label;
+   private String[] connectedCities;
+   private int[] travelTime;    
+   private int routeCount; 
+  
+public City(String label,int maxRoutes) {
+	this.label=label;
+	this.connectedCities=new String[maxRoutes];
+	this.travelTime=new int[maxRoutes];
+	this.routeCount=0;
 }
+
+public void addRoute(String city,int time) {
+	if (routeCount < connectedCities.length) {
+	connectedCities[routeCount]=city;
+	travelTime[routeCount]=time;
+	routeCount++;
+	}else {
+		System.out.println("Maximum number of routes reached! No more routes can be added.");
+	}
+}
+	
+public String getLabel() {
+	return label;
+}
+
+public String[] getConnectedCities() {
+	return connectedCities;
+}
+public int[] gettravelTime() {
+	return travelTime;
+}
+public int getRouteCount() {
+	return routeCount;
+}
+}
+
+  
