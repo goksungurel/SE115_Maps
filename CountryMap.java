ea02026425788868589a8f222bc3e54ae61affce
goksungurel <148645864+goksungurel@users.noreply.github.com>
2024-12-03 13:07:06 +0300

    Update CountryMap.java
    add a constructor , addCity() method , findCity() method

--- CountryMap.java
+++ CountryMap.java
@@ -1,3 +1,29 @@
 public class CountryMap{
+private City[] cities;
+  private int cityCount;
 
+public CountryMap(int numberOfCities) {
+	this.cities=new City[numberOfCities];
+	this.cityCount=0;
+}
+public void addCity(String label,int maxRoutes) {
+	if(cityCount<cities.length) {
+		cities[cityCount]=new City(label,maxRoutes);
+		cityCount++;
+	}
+}
+
+public City findCity(String label) {
+	for(int i=0;i<cityCount;i++) {
+		if(cities[i].getLabel().equals(label)) {
+			return cities[i];
+		}
+	
+  }
+	 System.out.println("Hata: " + label + " etiketiyle şehir bulunamadı.");
+	return null;
+}
+public City[] getCities() {
+	return cities;
+}
 }
